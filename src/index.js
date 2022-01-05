2b5ce7911b89ae3538604ee67011617f6df44eba
RyanDeLong <rdelong111@gmail.com>
2022-01-04 21:02:23 -0600

    commit tree 108785001218286c02ca6e0122cfc227687258e8

--- src/index.js
+++ src/index.js
@@ -13,3 +13,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const thecollection = document.getElementById('toy-collection');
+fetch('http://localhost:3000/toys')
+  .then((r) => r.json())
+  .then((object) => {
+    for (toy in object) {
+      addtoys(object[toy]);
+    }
+  });
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const toydata = {
+    name: document.querySelectorAll('input.input-text')[0].value,
+    image: document.querySelectorAll('input.input-text')[1].value,
+    likes: 0
+  };
+
+  const pushobject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(toydata)
+  };
+
+  fetch('http://localhost:3000/toys',pushobject)
+  .then((r) => r.json())
+  .then(() => {
+    addtoys(toydata);
+  });
+});
+
+function addtoys(toy) {
+  const newtoy = document.createElement('div');
+  const toyname = document.createElement('h2');
+  const toyimg = document.createElement('img');
+  const toylikes = document.createElement('p');
+  const toybtn = document.createElement('button');
+
+  newtoy.setAttribute('class', 'card');
+  toyname.textContent = toy.name;
+  toyimg.setAttribute('class', 'toy-avatar');
+  toyimg.setAttribute('src', toy.image);
+  toylikes.textContent = toy.likes;
+  toybtn.setAttribute('class', 'like-btn');
+  toybtn.setAttribute('id', toy.id);
+  toybtn.textContent = 'like';
+
+  newtoy.appendChild(toyname);
+  newtoy.appendChild(toyimg);
+  newtoy.appendChild(toylikes);
+  newtoy.appendChild(toybtn);
+  thecollection.appendChild(newtoy);
+
+  document.getElementById(`${toy.id}`).addEventListener('click', () => {
+    toy.likes++;
+    newtoy.querySelector('p').textContent = toy.likes;
+    updatelikes(toy);
+  });
+}
+
+function updatelikes(toy) {
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({'likes': toy.likes})
+  });
+}
